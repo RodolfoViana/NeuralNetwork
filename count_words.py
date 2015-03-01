d4a56063f059184c504db12dba6f1c297b29aecd
Rodolfo Viana <rodolfodelimaviana@gmail.com>
2015-03-01 18:51:33 -0300

    Count words and search query correct

---
+++ count_words.py
@@ -0,0 +1,39 @@
+"""
+Created on Mon Mar 01 1:09:00 2015
+
+@author: Rodolfo Viana
+"""
+
+# This program ranks popular word on the file
+
+import operator
+
+
+# Count words
+def count_words():
+    file_in = open("data_tweet.txt", "r")
+    file_out = open("words_count.txt", "w")
+
+    dict_words_num = {}
+
+    for tweet in file_in:
+        for word in tweet.split(",")[1].split("Text:")[1].split(" "):
+            if dict_words_num.has_key(word.lower()):
+                dict_words_num[word.lower()] += 1
+            else:
+                dict_words_num[word.lower()] = 0
+
+    dict_words_num = sorted(dict_words_num.items(), key=operator.itemgetter(1))
+
+    file_out.write(str(dict_words_num))
+
+    file_in.close()
+    file_out.close()
+
+# Main
+def main():
+    count_words()
+    pass
+
+if __name__ == '__main__':
+    main()
--- search_query.py
+++ search_query.py
@@ -1,8 +1,11 @@
 """
-Created on Mon Feb 03 20:10:35 2014
+Created on Mon Feb 23 20:10:35 2015
 
-@author: Rodolfo
+@author: Rodolfo Viana
 """
+
+# In this program it is possible search for tweets that say anything (bad or not bad) about Apple at Times Square.
+
 import twitter
 import sys
 import json
@@ -10,39 +13,65 @@ import json
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-myApi=twitter.Api(consumer_key='Q6KsZDoY5vNUAyeQhY1Xaw', consumer_secret='mvO4XXnPz2KLvPD6KR5N2S19a1CTiHkN8PKZRv1KQ', access_token_key='44794860-ShHgE1f3MI6TqJ5cyJt7DWTzQ8vVxjbaWC5MMGZSy',  access_token_secret='i2r27D09rJiwn4wNs1QBe0uRmsMTor2SzK0iV8e9AD006')
+# Load twitter api with consumer key and access token
+myApi = twitter.Api(consumer_key='Q6KsZDoY5vNUAyeQhY1Xaw', consumer_secret='mvO4XXnPz2KLvPD6KR5N2S19a1CTiHkN8PKZRv1KQ',
+                    access_token_key='44794860-ShHgE1f3MI6TqJ5cyJt7DWTzQ8vVxjbaWC5MMGZSy',
+                    access_token_secret='i2r27D09rJiwn4wNs1QBe0uRmsMTor2SzK0iV8e9AD006')
 
-def search_query():
-#    query = '(mcdonalds OR Mcdonalds) AND (happy OR sad)' # Query for mcdonalds
 
-    geo = ('40.76222', '-73.9844', '15mi') # Times Square (NYC)
-    query = 'apple OR Apple'
-    
-    
-    MAX_ID = None
+# Search for anything related to Apple at Times Square
+def search_query(set_Tweets):
+    geo = ('40.76222', '-73.9844', '15mi')  # Times Square (NYC)
+    query = '(apple OR iPhone OR iWatch OR iOS)'
+
+    max_id = None
     tweets = []
     K = 18
-    for it in range(K): # Retrieve up to K * 100 tweets
+    for it in range(K):  # Retrieve up to K * 100 tweets
         temp_tweets = [json.loads(str(raw_tweet)) for raw_tweet \
-                in myApi.GetSearch(query, geo, count = 100,
-                    max_id = MAX_ID)]#, result_type='recent')]
+                       in myApi.GetSearch(query, geo, count=100, max_id=max_id)] #result_type='recent')]
 
         tweets = tweets + temp_tweets
         print('Tweets retrieved: %d' % len(tweets))
         if temp_tweets:
-            MAX_ID = temp_tweets[-1]['id']
-            
-    file_out = open('DataTweet.txt','a+')
-    
+            max_id = temp_tweets[-1]['id']
+
+    file_id = open('id.txt', 'a+')
+    file_out = open('data_tweet.txt', 'a+')
+
+    count_new_tweets = 0
+    unique_tweets = set_Tweets
     for raw_tweet in tweets:
-        file_out.write('ID: ' + str(raw_tweet['id']) + ' -- ' + 'Text: ' + raw_tweet['text'] + '\n' )
-        
+        if unique_tweets.__contains__(str(raw_tweet['id'])):
+            pass
+        else:
+            unique_tweets.append(str(raw_tweet['id']))
+            file_out.write('Id: ' + str(raw_tweet['id']) + "," + 'Text: ' + raw_tweet['text'] + ' \n')
+            file_id.write(str(raw_tweet['id']) + '\n')
+            count_new_tweets += 1
+
+    print ('%d New Tweets' % count_new_tweets)
+    file_id.close()
     file_out.close()
 
 
+# Read file and return all tweets ID in a list of tweets ID
+def read_file(file_in):
+    file_in = open(file_in, "r")
+
+    set_tweets = []
+    for line in file_in:
+        set_tweets.append(str(line).split("\n")[0])
+
+    file_in.close()
+    return set_tweets
+
+
+# Main
 def main():
-    search_query()
+    search_query(read_file("id.txt"))
     pass
+
 
 if __name__ == '__main__':
     main()
