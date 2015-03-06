c5a7c30ae7886529a912eb79fdaec7791c28f718
Rodolfo Viana <rodolfodelimaviana@gmail.com>
2015-03-06 16:52:41 -0300

    Created Validation

--- label_tweet.py
+++ label_tweet.py
@@ -8,10 +8,10 @@ Created on Mon Mar 01 19:01:23 2015
 
 def label_tweet():
     file_in = open("data_tweet.txt", "r")
-    file_out = open("tweet_label.txt", "w")
+    file_out = open("tweet_label.txt", "a")
 
     for line in file_in:
-        label = raw_input(line.split(":")[2] + ": ")
+        label = raw_input(line.split("Text:")[1] + ": ")
         file_out.write(line + "," + label + "\n")
 
 
--- validation.py
+++ validation.py
@@ -1,3 +1,7 @@
+__author__ = 'rodolfo'
+
+"train/validate to find out how many epochs to train"
+
 import numpy as np
 import cPickle as pickle
 from math import sqrt
@@ -7,19 +11,20 @@ from pybrain.supervised.trainers import BackpropTrainer
 
 train_file = 'data/train.csv'
 validation_file = 'data/validation.csv'
-output_model_file = 'model.pkl'
+output_model_file = 'model_val.pkl'
 
-hidden_size = 50
-epochs = 300
+hidden_size = 100
+epochs = 1000
+continue_epochs = 10
+validation_proportion = 0.15
 
-# load data
+# load data, join train and validation files
 
 train = np.loadtxt( train_file, delimiter = ',' )
 validation = np.loadtxt( validation_file, delimiter = ',' )
-train = np.vstack((train, validation))
+train = np.vstack(( train, validation ))
 
 x_train = train[:,0:-1]
-
 y_train = train[:,-1]
 y_train = y_train.reshape( -1, 1 )
 
@@ -34,20 +39,10 @@ ds.setField( 'target', y_train )
 
 # init and train
 
-net = buildNetwork( input_size, hidden_size, target_size, bias = True )
+net = buildNetwork( input_size, hidden_size, target_size, bias= True )
 trainer = BackpropTrainer( net,ds )
 
-print "training for {} epochs...".format( epochs )
+train_mse, validation_mse = trainer.trainUntilConvergence( verbose = True, validationProportion = validation_proportion,
+	maxEpochs = epochs, continueEpochs = continue_epochs )
 
-for i in range( epochs ):
-	mse = trainer.train()
-	rmse = sqrt( mse )
-	print "training RMSE, epoch {}: {}".format( i + 1, rmse )
-	
 pickle.dump( net, open( output_model_file, 'wb' ))
-
-
-
-
-
-
